23be2efe94283d5bc9e95b79c50ac880a8e41d97
Andy Loftus <aloftus@gmail.com>
2017-07-27 15:23:08 -0500

    Save only empty directories in allfileslist.

--- bin/scandir.bash
+++ bin/scandir.bash
@@ -28,15 +28,12 @@ dirsonly=$outdir/dirsonly
 # Save empty dirs to a file
 # Pipe Dirnames to parallel to find all non-dir children
 # Save Filesize + Filename to a file
-#| tee <( grep '^2\0' | cut -d '' -f 2 > $emptydirs ) \
 find "$srcdir" $maxdepth -type d ! -name $'*[\x1-\x1f]*' -printf '%n\0%p\n' \
-| tee $dirsonly \
+| tee >( grep -Pa '^2\x0' > $emptydirs ) \
 | cut -d '' -f 2 \
 | parallel -d '\n' --joblog $outdir/mk_filelist.joblog \
     find '{}' -maxdepth 1 \\\( -type f -o -type l \\\) ! -name "\$'*[\\x1-\\x1f]*'" -printf '%s\\0%p\\n' >> $outfile
 echo "Filesystem scan, elapsed seconds: $SECONDS"
 
-#cat "$emptydirs" >> "$outfile"
-#parallel -a $dirsonly "grep '^2\0' | cut -d '' -f 2 > $emptydirs
-
-#rm -f "$dirsonly"
+cat "$emptydirs" >> "$outfile"
+rm -f "$emptydirs"
