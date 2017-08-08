f1a6b633c44808e13d8300d4c23e6f0734d0542f
Andy Loftus <aloftus@gmail.com>
2017-08-08 15:15:03 -0500

    Update for new globus API

--- bin/json2ini.py
+++ bin/json2ini.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 
-''' Read JSON from stdin
-    Convert to INI file with section named JSON
-    Save ini output to a temp file.
-    Print path to temp file.
-'''
 
 import sys
 import json
 import configparser
 import tempfile
 import os
+import argparse
+
+import pprint
+
+def process_cmdline():
+    desc = '''
+        Read JSON from stdin.
+        Convert to INI file with section named JSON.
+        Save ini output to a temp file.
+        Print path to temp file.
+    '''
+    parser = argparse.ArgumentParser( description=desc )
+    parser.add_argument( '--listelem', '-l', type=int, metavar='N',
+        help=( "Input dict has one key, 'DATA', which is a list. "
+               "json2ini should parse the Nth list element of DATA" ) )
+    defaults = { 'listelem': -1,
+               }
+    parser.set_defaults( **defaults )
+    return parser.parse_args()
+
 
 def insert_into_cfg( data, cfg ):
     valid_types = ( str, int, float, bool, type( None ) )
@@ -22,10 +37,23 @@ def insert_into_cfg( data, cfg ):
             except ( TypeError ) as e:
                 pass
 
-data = json.load(sys.stdin)
-cfg = configparser.ConfigParser()
-insert_into_cfg( data, cfg )
-fd, tmpfn = tempfile.mkstemp()
-with os.fdopen( fd, 'w') as fh:
-    cfg.write( fh )
-print( tmpfn )
+
+def run():
+    args = process_cmdline()
+    #pprint.pprint( args )
+    rawdata = json.load(sys.stdin)
+    cfg = configparser.ConfigParser()
+    data = rawdata
+    if args.listelem >= 0 :
+        #print( 'args.listelem: {}'.format( args.listelem ) )
+        data = rawdata[ 'DATA' ][ args.listelem ]
+    #pprint.pprint( data )
+    #raise SystemExit()
+    insert_into_cfg( data, cfg )
+    fd, tmpfn = tempfile.mkstemp()
+    with os.fdopen( fd, 'w') as fh:
+        cfg.write( fh )
+    print( tmpfn )
+
+if __name__ == '__main__':
+    run()
